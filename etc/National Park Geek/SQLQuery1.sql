e31a3f31cccf025a560078c557506a062f6cf4ee
Alastair Bull <alastair.bull@yahoo.com>
2018-03-15 17:09:28 -0400

    css still to go

---
+++ etc/National Park Geek/SQLQuery1.sql
@@ -0,0 +1,6 @@
+INSERT INTO survey_result (parkCode, emailAddress, state, activityLevel) VALUES ('CVNP', 'shane@email.com', 'HI', 'active');
+INSERT INTO survey_result (parkCode, emailAddress, state, activityLevel) VALUES ('YNP', 'Alastair@email.com', 'OH', 'active');
+INSERT INTO survey_result (parkCode, emailAddress, state, activityLevel) VALUES ('CVNP', 'dtrump@whitehouse.gov', 'DC', 'inactive');
+INSERT INTO survey_result (parkCode, emailAddress, state, activityLevel) VALUES ('GNP', 'steve@gmail.com', 'MI', 'sedentary');
+INSERT INTO survey_result (parkCode, emailAddress, state, activityLevel) VALUES ('YNP', 'johndoe@gmail.com', 'FLt', 'extremely active');
+
